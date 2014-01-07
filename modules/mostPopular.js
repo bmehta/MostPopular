f7a18e4bd5b408d8a8bcd28d0fc5a1bc0ec81816
Alon Motro <amotro@gannett.com>
2014-01-07 15:33:53 -0500

    added initial chartbeat implementation

--- modules/mostPopular.js
+++ modules/mostPopular.js
@@ -2,6 +2,16 @@
 define(['appHost', 'utilities/serviceTools'], function(appHost, serviceTools) {
 		var mostPopular = { server : appHost };
 
+
+
+		mostPopular.server.get("/mostPopular/domain/:domain/section/:ssts", function(req, res){
+			serviceTools.request(util.format(serviceTools.cofig.GetMostPopular, req.params.domain, req.params.ssts, req.query.sortBy), function(error, response, body){
+				var chartbeatResponse = JSON.parse(body);
+				charbeatResponse.pages.forEach(function(item, index)){
+					console.log(item.path);
+				});
+			});
+			
 		mostPopular.server.get("/mostPopular/:sstsString", function(req, res){
 
 			serviceTools.request("http://usatoday.accu-weather.com/widget/usatoday/weather-data.asp?location=" + req.params.sstsString, function(error, response, body){
